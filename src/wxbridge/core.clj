532c2c1a1b5435b2ec396f8bf455baf7431245aa
Art Goldhammer <art.goldhammer@gmail.com>
2019-11-14 08:17:47 -0500

    added misc files

---
+++ src/wxbridge/core.clj
@@ -0,0 +1,63 @@
+(ns wxbridge.core
+  (:require
+   [ring.adapter.jetty :as ra]
+   #_[ring.middleware.file :as rmf :refer [wrap-file]]
+   #_[ring.middleware.resource :as rmr :refer [wrap-resource]]
+   [ring.util.response :as resp]
+   [compojure.core :refer [defroutes GET]]
+   [compojure.route :as route]
+   [org.httpkit.client :as http]
+   [wxbridge.conf :as conf :refer [appid]])
+  (:gen-class))
+
+
+(def wxurl "https://api.openweathermap.org/data/2.5/forecast")
+
+(def options {:timeout 1000
+              :query-params {"units" "imperial"
+                             "appid" appid}})
+
+(def cities ["Boston" "Paris,FR"])
+
+(comment
+    @(http/get wxurl options
+              (fn [{:keys [status headers body error]}] ;; asynchronous response handling
+                (if error
+                  (println "Failed, exception is " error)
+                  (println "Async HTTP GET: " status)))))
+
+(defn make-options [city]
+  (assoc-in options [:query-params "q"] city))
+
+(defn get-city-data [city]
+  (http/get wxurl (make-options city)
+            (fn [{:keys [status headers body error]}] ;; asynchronous response handling
+                  (if error
+                    (println "Failed, exception is " error)
+                    (println "Async HTTP GET: " body))) ))
+
+#_(defn handle [city]
+  (let [{:keys [status body]} (get-city-data city)]
+    (prn "handle" status body)
+    (if (= status 200)
+      {:status 200
+       :headers {"Content-Type" "application/json"
+                 "Access-Control-Allow-Origin" "*"}
+       :body body}
+      {:status 404
+       :headers {"Content-Type" "text/html"
+                 "Access-Control-Allow-Origin" "*"}
+       :body "Weather server failure"})))
+
+#_(defroutes rts
+  (GET "/" [] (resp/resource-response "index.html" {:root "public"}))
+  (GET "/wx/:city" [city] (handle city))
+  (route/resources "/")
+  (route/files "public")
+  (route/not-found "Weather info not found"))
+
+#_(defn -main
+  []
+  (ra/run-jetty rts {:port 3000}))
+
+
