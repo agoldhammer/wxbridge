47d033cd87731718e4afa3418667b7c4637938f0
Art Goldhammer <art.goldhammer@gmail.com>
2019-11-14 15:04:07 -0500

    using core.async

--- src/wxbridge/core.clj
+++ src/wxbridge/core.clj
@@ -1,45 +1,59 @@
 (ns wxbridge.core
   (:require
-   [ring.adapter.jetty :as ra]
+   #_[ring.adapter.jetty :as ra]
    #_[ring.middleware.file :as rmf :refer [wrap-file]]
    #_[ring.middleware.resource :as rmr :refer [wrap-resource]]
+   [clojure.core.async :as async :refer [go >! <! chan]]
    [ring.util.response :as resp]
    [compojure.core :refer [defroutes GET]]
    [compojure.route :as route]
    [org.httpkit.client :as http]
+   [org.httpkit.server :as svr]
    [wxbridge.conf :as conf :refer [appid]])
   (:gen-class))
 
 
 (def wxurl "https://api.openweathermap.org/data/2.5/forecast")
 
-(def options {:timeout 1000
+(def options {:timeout 1500
               :query-params {"units" "imperial"
                              "appid" appid}})
 
-(def cities ["Boston" "Paris,FR"])
+(def categories {:test ["Boston" "Paris,FR", "Venice,IT"]})
 
 (def svrdb (atom {}))
 
-(comment
-    @(http/get wxurl options
-              (fn [{:keys [status headers body error]}] ;; asynchronous response handling
-                (if error
-                  (println "Failed, exception is " error)
-                  (println "Async HTTP GET: " status)))))
+(def outch (chan))
+
 
 (defn make-options [city]
   (assoc-in options [:query-params "q"] city))
 
-(defn get-city-data [city db]
+(defn get-city-data [city channel]
   (http/get wxurl (make-options city)
             (fn [{:keys [status headers body error]}] ;; asynchronous response handling
-              (prn status)
+              (prn "gcd" status headers)
+              (if error
+                (go (>! channel {city {:success false
+                               :error error}}))
+                (go (>! channel {city {:success true
+                               :wxdata body}}))))))
+
+#_(defn get-city-data [city db]
+  (http/get wxurl (make-options city)
+            (fn [{:keys [status headers body error]}] ;; asynchronous response handling
+              (prn "gcd" headers)
               (if error
                 (swap! db assoc city {:success false
                                       :error error})
                 (swap! db assoc city {:success true
                                       :wxdata body})))))
+
+(defn get-category-data [category db]
+  (let [cities (get categories category)]
+    (reset! svrdb {})
+    (doseq [city cities]
+      (get-city-data city db))))
 
 #_(defn handle [city]
     (let [{:keys [status body]} (get-city-data city)]
@@ -61,8 +75,23 @@
   (route/files "public")
   (route/not-found "Weather info not found"))
 
-#_(defn -main
+(defn app [req]
+  {:status  200
+   :headers {"Content-Type" "text/html"}
+   :body    "hello HTTP!"})
+
+(defn -main
   []
-  (ra/run-jetty rts {:port 3000}))
+  (svr/run-server app {:port 7070})
+  #_(ra/run-jetty rts {:port 3000}))
+
+;; -------------------------------------
+(comment
+  @(http/get wxurl options
+             (fn [{:keys [status headers body error]}] ;; asynchronous response handling
+               (if error
+                 (println "Failed, exception is " error)
+                 (println "Async HTTP GET: " status)))))
+
 
 
